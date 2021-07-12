9076f94426f26f37775e4e6118eae5df7017cdb9
WilsonMa17 <50428283+WilsonMa17@users.noreply.github.com>
2021-07-12 15:30:52 -0700

    Update flask.py

--- Card Game Maker/flask.py
+++ Card Game Maker/flask.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 from coolname import generate_slug
 import random
 
@@ -12,10 +14,30 @@ app.config['MYSQL_DB'] = 'flask'
 mysql = MySQL(app)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg=msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/portal', methods=['POST', 'GET'])
+def portal():
     if request.method == 'GET':
-        return render_template("index.html")
+        return render_template('portal.html')
     if request.method == 'POST':
         name = request.form['name']
         attack = request.form['attack']
@@ -38,11 +60,11 @@ def update():
     pass
 
 
-#Creates a randomized deck of cards with unique attributes
+# Creates a randomized deck of cards with unique attributes
 @app.route('/randomize', methods=['POST', 'GET'])
 def randomize():
     if request.method == 'GET':
-        return render_template("index.html")
+        return render_template("portal.html")
     if request.method == 'POST':
         for i in range(40):
             name = generate_slug(2)
@@ -53,9 +75,12 @@ def randomize():
             cursor.execute(''' INSERT INTO monster_cards VALUES(%s,%s,%s,%s)''', (name, attack, defense, cost))
             mysql.connection.commit()
             cursor.close()
-    return render_template("index.html")
+    return render_template("portal.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
