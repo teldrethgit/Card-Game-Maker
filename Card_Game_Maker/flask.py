206958934f45fb684a4d76f927f176b5987ef095
WilsonMa17 <50428283+WilsonMa17@users.noreply.github.com>
2021-07-15 14:06:45 -0700

    Initialize flask server
    initialize connection to database using SQLAlchemy/Flask
    
    created card class

---
+++ Card_Game_Maker/flask.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://capstone_u2021_otcg:CS467cardgame!@classmysql.engr.oregonstate.edu/capstone_u2021_otcg'
+
+db = SQLAlchemy(app)
+
+
+class Cards(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    attack = db.Column(db.Integer, unique=False, nullable=False)
+    defense = db.Column(db.Integer, unique=False, nullable=False)
+    cost = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Cards %r>' % self.id
