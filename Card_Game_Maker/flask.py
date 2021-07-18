231d9a038093491622fdcc2895507194b265f484
WilsonMa17 <50428283+WilsonMa17@users.noreply.github.com>
2021-07-17 17:32:07 -0700

    INSERT, DELETE, UPDATE
    added:
    
    insert route
    
    delete route
    
    update route

--- Card_Game_Maker/flask.py
+++ Card_Game_Maker/flask.py
@@ -21,11 +21,50 @@ class Users(db.Model):
    password = db.Column(db.String(15), unique=False, nullable=False)
 
 
-@app.route('/display-cards', methods=['GET'])
+@app.route('/cards', methods=['GET'])
 def get_data():
     data = Cards.query.all()
     return jsonify([str(card) for card in data])
 
+
+@app.route('/cards', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        attack = request.form['attack']
+        defense = request.form['defense']
+        cost = request.form['cost']
+        card_contents = Cards(name=name, attack=attack, defense=defense, cost=cost)
+        db.session.add(card_contents)
+        db.session.commit()
+        return redirect('/cards')
+    else:
+        return redirect('/cards')
+
+    
+@app.route('/cards/delete/<int:id>')
+def delete(id):
+    card_to_delete = Cards.query.get_or_404(id)
+    db.session.delete(card_to_delete)
+    db.session.commit()
+    return redirect('/cards')
+
+
+@app.route('cards/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    card = Cards.query.get_or_404(id)
+    if request.method == 'POST':
+        try:
+            card.name = request.form['name']
+            card.attack = request.form['attack']
+            card.defense = request.form['defense']
+            card.cost = request.form['cost']
+            db.session.commit()
+            return redirect('/cards')
+        except:
+            return 'There was an issue updating the card'
+    else:
+        return redirect('/cards')
 
 if __name__ == "__main__":
     db.create_all()
