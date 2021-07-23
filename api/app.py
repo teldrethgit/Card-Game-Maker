a7f59ab7e7db1ba952585969b50bb6bdca35f915
WilsonMa17 <50428283+WilsonMa17@users.noreply.github.com>
2021-07-23 16:36:26 -0700

    Added Login Sessions logic
    Using Flask-login, added session data for users , added games routes

--- api/app.py
+++ api/app.py
@@ -1,12 +1,27 @@
+from coolname import generate_slug
 from flask import Flask, render_template, request, redirect, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+import random
+import os
 
+# Database Configs
 app = Flask(__name__)
 cors = CORS(app)
+app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://capstone2021otcg:CS467cardgame!@osu-otcg-db.cs3ccyqxyr4e.us-west-1.rds.amazonaws.com/osu-otcg-db'
 
 db = SQLAlchemy(app)
+
+# Flask Login Requirements
+login_manager = LoginManager()
+login_manager.login_view = '/'
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
 
 class Cards(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -23,10 +38,10 @@ class Decks(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     image = db.Column(db.LargeBinary, unique=False, nullable=True)
     description = db.Column(db.Text, unique=False, nullable=True)
-    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(15), unique=False, nullable=False)
@@ -39,7 +54,7 @@ class Games(db.Model):
     total_turns = db.Column(db.Integer, unique=True, nullable=False)
     health_pool = db.Column(db.Integer, unique=False, nullable=False)
     time_limit = db.Column(db.Integer, unique=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -57,6 +72,45 @@ def signup():
             return ('', 201)
     
     
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        user = Users.query.filter_by(name=name).first()
+        remember = True
+        if not user or user.password != password:
+            return 'Incorrect credentials or user does not exist'
+        else:
+            login_user(user, remember=remember)
+            return ('', 200)
+
+        
+@app.route('/logout')
+def logout():
+    logout_user()
+    return ''
+    
+    
+@app.route('/games', methods=['GET','POST'])
+def games_get_post():
+    if request.method == 'GET':
+        data = Games.query.all()
+        return jsonify([{"name": game.name} for game in data])
+    
+    elif request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        total_turns = request.form['total_turns']
+        health_pool = request.form['health_pool']
+        time_limit = request.form['time_limit']
+        user_id = current_user.id
+        game_contents = Games(name=name, description=description, total_turns=total_turns, health_pool=health_pool, time_limit=time_limit ,user_id=user_id)
+        db.session.add(game_contents)
+        db.session.commit()
+        return ('', 204)
+    
+    
 @app.route('/cards', methods=['GET', 'POST'])
 def cards_get_post():
     if request.method == 'GET':
@@ -66,22 +120,27 @@ def cards_get_post():
     elif request.method == 'POST':
         name = request.form['name']
         attack = request.form['attack']
-        defense = request.form['defense']
         cost = request.form['cost']
-        card_contents = Cards(name=name, attack=attack, defense=defense, cost=cost)
+        description = request.form['description']
+        image = request.form['image']
+        deck = request.form['deck']
+        card_contents = Cards(name=name, attack=attack, defense=defense, cost=cost,description=description,image=image,deck=deck)
         db.session.add(card_contents)
         db.session.commit()
         return ('', 204)
 
+    
 @app.route('/cards/<int:id>', methods=['PUT', 'DELETE'])
 def cards_put_delete(id):
     if request.method == 'PUT':
         card = Cards.query.get_or_404(id)
         try:
-            card.name = request.form['name']
-            card.attack = request.form['attack']
-            card.defense = request.form['defense']
-            card.cost = request.form['cost']
+            name = request.form['name']
+            attack = request.form['attack']
+            cost = request.form['cost']
+            description = request.form['description']
+            image = request.form['image']
+            deck = request.form['deck']
             db.session.commit()
             return ('', 204)
         except:
